c280d209e341bfff66e6a5361a8f5568a90075ee
JCShiro <jacob.shadbolt@student.norwichuni.ac.uk>
2026-02-11 12:30:42 +0000

    Updates

--- Assets/Scripts/ModelManager.cs
+++ Assets/Scripts/ModelManager.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 public class ModelManager : MonoBehaviour
 {
     public GameObject[] gameObjects;
-    float timestamp;
-    public float spawnRate;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Vector3 position = new Vector3(0, 0, 0);
-        Quaternion rotation = quaternion.AxisAngle(0, 0);
-        GameObject scene = Instantiate(gameObjects[0], position, rotation);
+        Quaternion rotation = Quaternion.AngleAxis(0, transform.up);
+        GameObject myObject = Instantiate(gameObjects[0], position, rotation);
+        print("myObject = " +myObject);
     }
 
     // Update is called once per frame
