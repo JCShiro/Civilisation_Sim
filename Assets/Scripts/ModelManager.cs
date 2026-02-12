bd96e15436d357115b4fa6b391e30b124d247150
JCShiro <jacob.shadbolt@student.norwichuni.ac.uk>
2026-02-12 16:30:26 +0000

    upgrade

--- Assets/Scripts/ModelManager.cs
+++ Assets/Scripts/ModelManager.cs
@@ -3,19 +3,31 @@ using UnityEngine;
 
 public class ModelManager : MonoBehaviour
 {
+    GameObject myObject;
+    int modelIndex = 0;
     public GameObject[] gameObjects;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Vector3 position = new Vector3(0, 0, 0);
-        Quaternion rotation = Quaternion.AngleAxis(0, transform.up);
-        GameObject myObject = Instantiate(gameObjects[0], position, rotation);
-        print("myObject = " +myObject);
+        myObject = Instantiate(gameObjects[modelIndex], transform);
+        print("myObject = " + myObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void UpdateModel()
+    {
+        Destroy(myObject);
+        modelIndex++;
+        if (modelIndex >= gameObjects.Length)
+        {
+            modelIndex = 0;
+        }
+        // myObject = Instantiate(gameObjects[modelIndex], transform);
+        print("model index = " + modelIndex);
     }
 }
